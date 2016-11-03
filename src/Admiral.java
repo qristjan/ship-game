6ff6a831745f041649ae3cdbf8da102cd4d1f4dd
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2016-11-03 23:16:24 +0200

    first commit

---
+++ src/Admiral.java
@@ -0,0 +1,12 @@
+
+public class Admiral { //Paluti konstruktorit, siin see on!
+    String nimi;
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public Admiral(String nimi) {
+        this.nimi = nimi;
+    }
+}
